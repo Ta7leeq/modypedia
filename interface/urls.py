8eb9d79e7600e994f5ace060d0de8d50c51c4eeb
Mohamed Basiouny <mawad.gap@gmail.com>
2024-11-20 14:33:06 +0100

    play button added

--- interface/urls.py
+++ interface/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import item_list,memory
+from .views import item_list,memory,details
 
 urlpatterns = [
-    path('items/', item_list, name='item_list'),
+    path('', item_list, name='item_list'),
     path('memory/', memory, name='memory'),
+    path('details/<int:item_id>/', details, name='details'),
+    
     # Add other paths as needed
 ]
